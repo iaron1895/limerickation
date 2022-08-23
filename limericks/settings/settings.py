03cdd881c097a6e6db80eb92093585dbfc19fbbc
Inés Arondel Mugica <inesarondelmugica@iness-air.home>
2022-08-23 10:13:00 +0100

    Remove django heroku settings

--- limericks/settings/settings.py
+++ limericks/settings/settings.py
@@ -113,6 +113,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-django_heroku.settings(locals())
