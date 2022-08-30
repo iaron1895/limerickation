0b62c5014be799ced63f530531e77679fe2329b8
Inés Arondel Mugica <inesarondelmugica@iness-air.home>
2022-08-30 09:22:29 +0100

    Comment out download gpt-2 from startup fix indentation

--- limerines/management/commands/start_up.py
+++ limerines/management/commands/start_up.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
         except:
             raise CommandError('Initalization failed.')
 
-       """ try:
+        """ try:
             MODEL = GPT2LMHeadModel.from_pretrained('limerines/gpt2_models/distilgpt2-model')
         except:
             MODEL = GPT2LMHeadModel.from_pretrained('distilgpt2')
